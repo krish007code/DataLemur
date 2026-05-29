a706b90555c51966fc37327a9f631f04d7e8fbbb
Kavyansh Kumar Baghel <Kavyanshkumarbaghel@gmail.com>
2026-05-29 15:56:52 +0530

    Create Tweets' Rolling Averages.sql

---
+++ Tweets' Rolling Averages.sql
@@ -0,0 +1,5 @@
+SELECT
+  user_id,
+  tweet_date,
+  ROUND(AVG(tweet_count) over(PARTITION by user_id order by tweet_date ROWs BETWEEN 2 PRECEDING AND CURRENT ROW), 2)as rolling_avg_3d
+FROM tweets;
